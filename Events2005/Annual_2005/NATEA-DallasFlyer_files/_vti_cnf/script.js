eba28423275180859adfbc329b497a52f8af0c97
David Yen <david.yen@argodata.com>
2015-03-25 17:01:51 -0500

    initial commit

---
+++ Events2005/Annual_2005/NATEA-DallasFlyer_files/_vti_cnf/script.js
@@ -0,0 +1,5 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|12 Nov 2005 16:36:49 -0000
+vti_extenderversion:SR|5.0.2.2623
+vti_thicketsupportingfile:BW|true
+vti_backlinkinfo:VX|documents/NATEA-DallasFlyer_files/frame.htm documents/NATEA-DallasFlyer_files/master01.htm documents/NATEA-DallasFlyer_files/master03.htm documents/NATEA-DallasFlyer_files/outline.htm documents/NATEA-DallasFlyer_files/fullscreen.htm documents/NATEA-DallasFlyer_files/slide0004.htm documents/NATEA-DallasFlyer_files/slide0002.htm
